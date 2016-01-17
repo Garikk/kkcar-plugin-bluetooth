52f7753a4f0795a0063522da2321cf99614d898a
Garikk <supergarikk@gmail.com>
2016-01-17 20:20:00 +0300

    fix

--- src/main/java/kkdev/kksystem/plugin/bluetooth/configuration/kk_DefaultConfig.java
+++ src/main/java/kkdev/kksystem/plugin/bluetooth/configuration/kk_DefaultConfig.java
@@ -5,6 +5,7 @@
  */
 package kkdev.kksystem.plugin.bluetooth.configuration;
 
+import static kkdev.kksystem.base.constants.PluginConsts.KK_PLUGIN_BASE_PLUGIN_BLUETOOTH_SERVICE_RFCOMM_UUID;
 import static kkdev.kksystem.base.constants.PluginConsts.KK_PLUGIN_BASE_PLUGIN_EXTCONNECTOR_BTSERVICE_UUID;
 
 
@@ -20,15 +21,14 @@ public abstract class kk_DefaultConfig {
         BTConfig DefConf = new BTConfig();
         
         DefConf.ODBAdapter=BTConfig.AdapterTypes.BlueCove;
-        DefConf.ServiceUUID=new long[1];
-        DefConf.ServiceUUID[0]=KK_PLUGIN_BASE_PLUGIN_BLUETOOTH_SERVICE_RFCOMM_UUID;
         //
         DefConf.BTServicesMapping=new ServicesConfig[1];
         DefConf.BTServicesMapping[0]=new ServicesConfig();
         DefConf.BTServicesMapping[0].SourceType=ServicesConfig.BT_ServiceType.RFCOMM;
         DefConf.BTServicesMapping[0].TargetTag="RAWGPS";
         DefConf.BTServicesMapping[0].SourceAddr=""; //any
-        
+        DefConf.BTServicesMapping[0].ServicesUUID=new long[1];
+        DefConf.BTServicesMapping[0].ServicesUUID[0]=KK_PLUGIN_BASE_PLUGIN_BLUETOOTH_SERVICE_RFCOMM_UUID;
         return DefConf;
     }
 }
